96dadb2e8feef109c0197ed0c690c2ceee8e335f
Tabea <tabea.eggler@students.fhnw.ch>
2020-03-19 16:02:55 +0100

    added project structure

---
+++ mission-erde/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import "./components/test/TestComponent";
+import TestComponent from "./components/test/TestComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hallo React-App</p>
+      <TestComponent />
+    </div>
+  );
+}
+
+export default App;
