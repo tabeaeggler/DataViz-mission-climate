61ea7ea016df37ebf303f8345f5587cc2f15498d
Tabea <tabea.eggler@students.fhnw.ch>
2020-03-22 11:37:33 +0100

    configuring prettier

--- web-app/src/components/test/BasicLineGraph.js
+++ web-app/src/components/test/BasicLineGraph.js
@@ -1,6 +1,6 @@
 //useRef hook: creates a variable in a function to hold an object across multiple rendering passes.
 //useEffect hook: Used when DOM-elements have been rendered -> Similar to componentDidMount
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react"
 import {
   select,
   line,
@@ -8,42 +8,42 @@ import {
   axisBottom,
   scaleLinear,
   axisRight
-} from "d3";
-import "./basicLineGraph.css";
+} from "d3"
+import "./basicLineGraph.css"
 
 function BasicLineGraph() {
-  const [data, setData] = useState([20, 30, 60, 25, 10, 18, 20, 20, 90, 10]);
-  const svgRef = useRef();
+  const [data, setData] = useState([20, 30, 60, 25, 10, 18, 20, 20, 90, 10])
+  const svgRef = useRef()
 
   useEffect(() => {
-    const svg = select(svgRef.current); //wrapper, so that the svg is available for d3.
+    const svg = select(svgRef.current) //wrapper, so that the svg is available for d3.
 
     const xScale = scaleLinear()
       .domain([0, data.length - 1])
-      .range([0, 300]); //visual representation of domain
+      .range([0, 300]) //visual representation of domain
 
     const yScale = scaleLinear()
       .domain([0, 90]) //max element of data array
-      .range([150, 0]);
+      .range([150, 0])
 
     const xAxis = axisBottom(xScale)
       .ticks(data.length)
-      .tickFormat(index => index + 1);
+      .tickFormat(index => index + 1)
     svg
       .select(".x-axis")
       .style("transform", "translateY(150px)")
-      .call(xAxis);
+      .call(xAxis)
 
-    const yAxis = axisRight(yScale);
+    const yAxis = axisRight(yScale)
     svg
       .select(".y-axis")
       .style("transform", "translateX(300px)")
-      .call(yAxis);
+      .call(yAxis)
 
     const myLine = line()
       .x((value, index) => xScale(index))
       .y(yScale)
-      .curve(curveCardinal);
+      .curve(curveCardinal)
 
     svg
       .selectAll(".line")
@@ -52,8 +52,8 @@ function BasicLineGraph() {
       .attr("class", "line")
       .attr("d", value => myLine(value))
       .attr("fill", "none")
-      .attr("stroke", "blue");
-  }, [data]); //[]: if array empty: ensure that it only runs once.
+      .attr("stroke", "blue")
+  }, [data]) //[]: if array empty: ensure that it only runs once.
 
   return (
     <React.Fragment>
@@ -67,10 +67,10 @@ function BasicLineGraph() {
         Update data
       </button>
     </React.Fragment>
-  );
+  )
 }
 
-export default BasicLineGraph;
+export default BasicLineGraph
 
 /** Part 1: Render some dots
     svg
