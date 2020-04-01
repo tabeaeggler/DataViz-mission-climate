8fe653c10fb9da0f73e5e589b5d79589a9226271
Hannah <hannahsarah.kuehne@students.fhnw.ch>
2020-04-01 16:53:09 +0200

    basic translation working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
-import "./App.css";
-import BasicLineGraph from "./components/test/BasicLineGraph";
+import React from "react"
+import "./App.css"
+import { useTranslation } from "react-i18next"
+import BasicLineGraph from "./components/test/BasicLineGraph"
 
 function App() {
+  const { t, i18n } = useTranslation()
+  function handleClick(lang) {
+    i18n.changeLanguage(lang)
+  }
+
   return (
     <div className="App">
-      <p>Hallo React-App</p>
       <BasicLineGraph />
+      <button onClick={() => handleClick("en")}>E</button>
+      <button onClick={() => handleClick("de")}>D</button>
+      <button onClick={() => handleClick("fr")}>F</button>
+      <button onClick={() => handleClick("it")}>I</button>
+      <p>{t("Willkommen.1")}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
