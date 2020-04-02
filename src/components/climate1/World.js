129eb413d5b26905227a4b3bf7264a80ad5887be
Hannah <hannahsarah.kuehne@students.fhnw.ch>
2020-04-02 17:30:24 +0200

    first styling

--- src/components/climate1/World.js
+++ src/components/climate1/World.js
@@ -1,7 +1,7 @@
 import Globe from "react-globe.gl"
 import React, { useState, useEffect } from "react"
 import { useTranslation } from "react-i18next"
-import { scaleSequentialSqrt, interpolateYlOrRd } from "d3"
+import { scaleSequential, interpolateYlOrRd } from "d3"
 
 const World = () => {
   // const { t, i18n } = useTranslation()
@@ -10,36 +10,35 @@ const World = () => {
   // }
 
   const [countries, setCountries] = useState({ features: [] })
-  const [hoverD, setHoverD] = useState()
+  const [clickedCountry, setClickedCountry] = useState()
 
   useEffect(() => {
     // load data
     fetch(
-      "https://raw.githubusercontent.com/tabeaeggler/geojson/master/geojson_temperature_str.geojson"
+      "https://raw.githubusercontent.com/tabeaeggler/geojson/master/geojson_temperature.geojson"
     )
       .then(res => res.json())
       .then(setCountries)
   }, [])
 
-  const colorScale = scaleSequentialSqrt(interpolateYlOrRd).domain([0, 3])
+  const colorScale = scaleSequential(interpolateYlOrRd).domain([0, 3])
 
   //temperature values
   const getVal = feat => feat.properties.TEMP
 
   return (
     <Globe
+      showGraticules="true"
       polygonsData={countries.features}
-      polygonAltitude={d => (d === hoverD ? 0.12 : 0.06)}
-      polygonCapColor={d =>
-        d === hoverD ? "steelblue" : colorScale(getVal(d))
-      }
-      polygonSideColor={() => "rgba(0, 100, 0, 0.15)"}
+      polygonAltitude={d => (d === clickedCountry ? 0.12 : 0.06)}
+      polygonCapColor={d => colorScale(getVal(d))}
+      polygonSideColor={() => "rgba(0, 0, 0, 0.2)"}
       polygonStrokeColor={() => "#111"}
       polygonLabel={({ properties: d }) => `
-        <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
-        TEMP: <i>${d.TEMP}</i> M$<br/>
+        <b>${d.ADMIN}:</b> <br />
+        TEMP: <i>${d.TEMP}</i><br/>
       `}
-      onPolygonHover={setHoverD}
+      onPolygonClick={setClickedCountry}
       polygonsTransitionDuration={300}
     />
   )
