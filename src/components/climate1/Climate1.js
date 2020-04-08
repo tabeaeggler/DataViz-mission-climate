73f7a6918d4e4721a7169cf651aaae448703ba63
Hannah <hannahsarah.kuehne@students.fhnw.ch>
2020-04-08 10:49:20 +0200

    added title and comments in translation file

--- src/components/climate1/Climate1.js
+++ src/components/climate1/Climate1.js
@@ -1,10 +1,13 @@
 import React from "react"
 import World from "./World"
 import "./climate1.css"
+import { useTranslation } from "react-i18next"
 
 function Climate1() {
+  const { t } = useTranslation()
   return (
     <div>
+      <h1> {t("Climate1_Title.1")}</h1>
       <World />
     </div>
   )
