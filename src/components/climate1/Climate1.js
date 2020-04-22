0594dd9519c783cc56715c397806aaeb3164563d
Tabea <tabea.eggler@students.fhnw.ch>
2020-04-22 21:25:15 +0200

    try css transition

--- src/components/climate1/Climate1.js
+++ src/components/climate1/Climate1.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from "react"
 import World from "./World"
 import "./climate1.css"
 import { useTranslation } from "react-i18next"
+import { CSSTransition } from "react-transition-group"
 
 function Climate1() {
   const { t } = useTranslation()
@@ -12,66 +13,80 @@ function Climate1() {
 
   function createBubbleGlobe() {
     return (
-      <div className={showBubble.globe ? "bubble-visible" : "bubble-hidden"}>
-        <div className="bubble-box-climate1-globe">
-          <p>{t("Climate1_Bubble.1")}</p>
-          <button
-            className="bubble-button"
-            onClick={() => setShowBubble({ globe: false, linegraph: true })}>
-            <p>
-              {t("Climate1_Button.1")}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="bubble-button-arrow-globe"
-                viewBox="0 0 511.995 511.995">
-                <path
-                  d="M381.039,248.62L146.373,3.287c-4.083-4.229-10.833-4.417-15.083-0.333c-4.25,4.073-4.396,10.823-0.333,15.083
+      <CSSTransition
+        in={showBubble.globe}
+        timeout={800}
+        classNames="bubble-fade-in"
+        unmountOnExit
+        appear>
+        <div className={showBubble.globe ? "bubble-visible" : "bubble-hidden"}>
+          <div className="bubble-box-climate1-globe">
+            <p>{t("Climate1_Bubble.1")}</p>
+            <button
+              className="bubble-button"
+              onClick={() => setShowBubble({ globe: false, linegraph: true })}>
+              <p>
+                {t("Climate1_Button.1")}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="bubble-button-arrow-globe"
+                  viewBox="0 0 511.995 511.995">
+                  <path
+                    d="M381.039,248.62L146.373,3.287c-4.083-4.229-10.833-4.417-15.083-0.333c-4.25,4.073-4.396,10.823-0.333,15.083
                   L358.56,255.995L130.956,493.954c-4.063,4.26-3.917,11.01,0.333,15.083c2.063,1.979,4.729,2.958,7.375,2.958
                   c2.813,0,5.604-1.104,7.708-3.292L381.039,263.37C384.977,259.245,384.977,252.745,381.039,248.62z"
-                />
-              </svg>
-            </p>
-          </button>
+                  />
+                </svg>
+              </p>
+            </button>
+          </div>
+          <svg
+            className="bubble-arrow-climate1-globe"
+            xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 42.5L44 2V85.5L1 42.5Z" />
+          </svg>
         </div>
-        <svg
-          className="bubble-arrow-climate1-globe"
-          xmlns="http://www.w3.org/2000/svg">
-          <path d="M1 42.5L44 2V85.5L1 42.5Z" />
-        </svg>
-      </div>
+      </CSSTransition>
     )
   }
 
   function createBubbleLineGraph() {
     return (
-      <div
-        className={showBubble.linegraph ? "bubble-visible" : "bubble-hidden"}>
-        <div className="bubble-box-climate1-linegraph">
-          <p>{t("Climate1_Bubble.2")}</p>
-          <button
-            className="bubble-button"
-            onClick={() => setShowBubble({ globe: false, linegraph: true })}>
-            <p>
-              {t("Climate1_Button.2")}{" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="bubble-button-arrow-linegraph"
-                viewBox="0 0 511.995 511.995">
-                <path
-                  d="M381.039,248.62L146.373,3.287c-4.083-4.229-10.833-4.417-15.083-0.333c-4.25,4.073-4.396,10.823-0.333,15.083
+      <CSSTransition
+        in={showBubble.linegraph}
+        timeout={800}
+        classNames="bubble-fade-in"
+        unmountOnExit
+        appear>
+        <div
+          className={showBubble.linegraph ? "bubble-visible" : "bubble-hidden"}>
+          <div className="bubble-box-climate1-linegraph">
+            <p>{t("Climate1_Bubble.2")}</p>
+            <button
+              className="bubble-button"
+              onClick={() => setShowBubble({ globe: false, linegraph: true })}>
+              <p>
+                {t("Climate1_Button.2")}{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="bubble-button-arrow-linegraph"
+                  viewBox="0 0 511.995 511.995">
+                  <path
+                    d="M381.039,248.62L146.373,3.287c-4.083-4.229-10.833-4.417-15.083-0.333c-4.25,4.073-4.396,10.823-0.333,15.083
                   L358.56,255.995L130.956,493.954c-4.063,4.26-3.917,11.01,0.333,15.083c2.063,1.979,4.729,2.958,7.375,2.958
                   c2.813,0,5.604-1.104,7.708-3.292L381.039,263.37C384.977,259.245,384.977,252.745,381.039,248.62z"
-                />
-              </svg>
-            </p>
-          </button>
+                  />
+                </svg>
+              </p>
+            </button>
+          </div>
+          <svg
+            className="bubble-arrow-climate1-linegraph"
+            xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 42.5L44 2V85.5L1 42.5Z" />
+          </svg>
         </div>
-        <svg
-          className="bubble-arrow-climate1-linegraph"
-          xmlns="http://www.w3.org/2000/svg">
-          <path d="M1 42.5L44 2V85.5L1 42.5Z" />
-        </svg>
-      </div>
+      </CSSTransition>
     )
   }
 
