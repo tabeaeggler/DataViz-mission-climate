a78e25ee56278a07b4dc69ef6d8561faf260d5e5
Tabea <tabea.eggler@students.fhnw.ch>
2020-04-23 10:51:42 +0200

    bubbly layout try

--- src/components/climate1/World.js
+++ src/components/climate1/World.js
@@ -151,7 +151,7 @@ const World = () => {
     return (
       <CSSTransition
         in={showBubble.globe}
-        timeout={3000}
+        timeout={4000}
         classNames="bubble-fade"
         unmountOnExit
         appear>
@@ -166,26 +166,14 @@ const World = () => {
                   linegraph: true,
                 })
               }>
-              <p>
-                {t("Climate1_Button.1")}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="bubble-button-arrow-globe"
-                  viewBox="0 0 511.995 511.995">
-                  <path
-                    d="M381.039,248.62L146.373,3.287c-4.083-4.229-10.833-4.417-15.083-0.333c-4.25,4.073-4.396,10.823-0.333,15.083
-                  L358.56,255.995L130.956,493.954c-4.063,4.26-3.917,11.01,0.333,15.083c2.063,1.979,4.729,2.958,7.375,2.958
-                  c2.813,0,5.604-1.104,7.708-3.292L381.039,263.37C384.977,259.245,384.977,252.745,381.039,248.62z"
-                  />
-                </svg>
-              </p>
+              <p>{t("Climate1_Button.1")}</p>
             </button>
+            <svg
+              className="bubble-arrow-climate1-globe"
+              xmlns="http://www.w3.org/2000/svg">
+              <path d="M1 42.5L44 2V85.5L1 42.5Z" />
+            </svg>
           </div>
-          <svg
-            className="bubble-arrow-climate1-globe"
-            xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 42.5L44 2V85.5L1 42.5Z" />
-          </svg>
         </div>
       </CSSTransition>
     )
@@ -198,7 +186,7 @@ const World = () => {
     return (
       <CSSTransition
         in={showBubble.linegraph}
-        timeout={3000}
+        timeout={4000}
         classNames="bubble-fade"
         unmountOnExit
         appear>
@@ -213,19 +201,7 @@ const World = () => {
                   linegraph: true,
                 })
               }>
-              <p>
-                {t("Climate1_Button.2")}{" "}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="bubble-button-arrow-linegraph"
-                  viewBox="0 0 511.995 511.995">
-                  <path
-                    d="M381.039,248.62L146.373,3.287c-4.083-4.229-10.833-4.417-15.083-0.333c-4.25,4.073-4.396,10.823-0.333,15.083
-                  L358.56,255.995L130.956,493.954c-4.063,4.26-3.917,11.01,0.333,15.083c2.063,1.979,4.729,2.958,7.375,2.958
-                  c2.813,0,5.604-1.104,7.708-3.292L381.039,263.37C384.977,259.245,384.977,252.745,381.039,248.62z"
-                  />
-                </svg>
-              </p>
+              <p>{t("Climate1_Button.2")} </p>
             </button>
           </div>
           <svg
