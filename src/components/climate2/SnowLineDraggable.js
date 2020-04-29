e829e856eb271082273dc2f5bb8f7a6dcc8a578b
Tabea <tabea.eggler@students.fhnw.ch>
2020-04-29 15:00:02 +0200

    set boundaries of drag area

--- src/components/climate2/SnowLineDraggable.js
+++ src/components/climate2/SnowLineDraggable.js
@@ -20,7 +20,7 @@ function SnowLineDraggable() {
       `translate(${margin},${margin})`
     )
 
-    const yScale = scaleLinear().domain([0, mountainHeight]).range([0, height])
+    const yScale = scaleLinear().domain([mountainHeight, 0]).range([0, height])
 
     //add mountain
     svg
@@ -45,30 +45,36 @@ function SnowLineDraggable() {
       .attr("stroke", "green")
       .attr("x1", 0)
       .attr("x2", width)
-      .attr("y1", yScale(mountainHeight - data[0].snowline))
-      .attr("y2", yScale(mountainHeight - data[0].snowline))
+      .attr("y1", yScale(data[0].snowline))
+      .attr("y2", yScale(data[0].snowline))
 
     svg
       .append("line")
       .attr("stroke", "red")
       .attr("x1", 0)
       .attr("x2", width)
-      .attr("y1", yScale(mountainHeight - data[1].snowline))
-      .attr("y2", yScale(mountainHeight - data[1].snowline))
+      .attr("y1", yScale(data[1].snowline))
+      .attr("y2", yScale(data[1].snowline))
 
     //draggable line
-
     svg
       .append("line")
       .attr("stroke", "blue")
       .attr("stroke-width", "10")
       .attr("x1", 0)
       .attr("x2", width)
-      .attr("y1", yScale(mountainHeight - data[0].snowline - 100))
-      .attr("y2", yScale(mountainHeight - data[0].snowline - 100))
+      .attr("y1", yScale(data[0].snowline - 100))
+      .attr("y2", yScale(data[0].snowline - 100))
       .call(
         drag().on("start", dragstarted).on("drag", dragged).on("end", dragended)
       )
+
+    var text = svg
+      .append("text")
+      .style("fill", "white")
+      .attr("x", width + 20)
+      .attr("y", yScale(data[0].snowline - 100))
+      .text(data[0].snowline + " m.ü.M")
 
     function dragstarted() {
       select(this).classed("active-d3-item", true)
@@ -76,13 +82,20 @@ function SnowLineDraggable() {
 
     function dragged() {
       var y = event.dy
-
       var currentLine = select(this)
+      var newYPosition = parseInt(currentLine.attr("y1")) + y
+      console.log(newYPosition)
+
+      //Check boundary
+      if (newYPosition > height) newYPosition = height
+      else if (newYPosition < 0) newYPosition = 0
 
       // Update the line properties
-      currentLine
-        .attr("y1", parseInt(currentLine.attr("y1")) + y)
-        .attr("y2", parseInt(currentLine.attr("y2")) + y)
+      currentLine.attr("y1", newYPosition).attr("y2", newYPosition)
+
+      text
+        .attr("y", newYPosition)
+        .text(yScale.invert(newYPosition).toFixed(0) + " m.ü.M")
     }
 
     function dragended() {
@@ -99,9 +112,7 @@ function SnowLineDraggable() {
 
   return (
     <React.Fragment>
-      <svg
-        className="snowLine-container"
-        width={(window.innerWidth / 2) * 0.93}>
+      <svg className="snowLine-container" width={500}>
         <g ref={svgRef}></g>
       </svg>
     </React.Fragment>
