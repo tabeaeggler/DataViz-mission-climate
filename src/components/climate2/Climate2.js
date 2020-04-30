4c706b9fc9942519e8fe2ef9c5f6d9cdd2ab2e37
Hannah <hannahsarah.kuehne@students.fhnw.ch>
2020-04-30 13:54:42 +0200

    integrated snow storm library

--- src/components/climate2/Climate2.js
+++ src/components/climate2/Climate2.js
@@ -2,21 +2,15 @@ import React from "react"
 import SnowLineOverview from "./SnowLineOverview"
 import "./climate2.css"
 import { useTranslation } from "react-i18next"
+import Snow from "react-snowstorm"
 
 function Climate2() {
   const { t } = useTranslation()
 
   return (
     <React.Fragment>
-      <div class="snow-container">
+      <Snow animationInterval={70}></Snow>
       <SnowLineOverview />
-        <div class="snow foreground"></div>
-        <div class="snow foreground layered"></div>
-        <div class="snow middleground"></div>
-        <div class="snow middleground layered"></div>
-        <div class="snow background"></div>
-        <div class="snow background layered"></div>
-      </div>
     </React.Fragment>
   )
 }
