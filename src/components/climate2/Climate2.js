e67ac8c7e97f2da1d7d99508e191d831283fde38
Hannah <hannahsarah.kuehne@students.fhnw.ch>
2020-04-30 13:05:28 +0200

    snow with css animation

--- src/components/climate2/Climate2.js
+++ src/components/climate2/Climate2.js
@@ -8,7 +8,15 @@ function Climate2() {
 
   return (
     <React.Fragment>
+      <div class="snow-container">
       <SnowLineOverview />
+        <div class="snow foreground"></div>
+        <div class="snow foreground layered"></div>
+        <div class="snow middleground"></div>
+        <div class="snow middleground layered"></div>
+        <div class="snow background"></div>
+        <div class="snow background layered"></div>
+      </div>
     </React.Fragment>
   )
 }
