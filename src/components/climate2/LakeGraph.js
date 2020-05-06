2297f4793e24ee00f4b9547e3d830d226009a344
Hannah <hannahsarah.kuehne@students.fhnw.ch>
2020-05-06 14:56:07 +0200

    improved LakeGraph

--- src/components/climate2/LakeGraph.js
+++ src/components/climate2/LakeGraph.js
@@ -5,8 +5,8 @@ import FilledLake from "../../assets/img/filledLakeOfLucern.svg"
 const LakeGraph = props => {
   const originalScale = 6
   const screenWidth = window.innerWidth
-  const screenHeight = window.innerHeight
-
+    const screenHeight = window.innerHeight
+    
   return (
     <React.Fragment>
       <div className="lake-graph-container">
@@ -14,13 +14,15 @@ const LakeGraph = props => {
           <img
             width={screenWidth / originalScale}
             height={screenHeight / originalScale}
-            src={Lake}></img>
+            src={Lake}
+            alt="original lake"></img>
         </div>
         <div className="scaled-lake">
           <img
             width={(screenWidth / originalScale) * props.scaleFactor}
             height={(screenHeight / originalScale) * props.scaleFactor}
-            src={FilledLake}></img>
+            src={FilledLake}
+            alt="scaled lake"></img>
         </div>
       </div>
     </React.Fragment>
