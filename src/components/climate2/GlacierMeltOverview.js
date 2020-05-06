822cb91c4eff11a9d822d281dd22966982fa0f86
Tabea <tabea.eggler@students.fhnw.ch>
2020-05-06 12:20:32 +0200

    slider with labels

--- src/components/climate2/GlacierMeltOverview.js
+++ src/components/climate2/GlacierMeltOverview.js
@@ -2,6 +2,10 @@ import React, { useEffect, useRef, useState } from "react"
 import { useTranslation } from "react-i18next"
 import { CSSTransition } from "react-transition-group"
 import ButtonRight from "../../assets/img/buttonRight.svg"
+import Slider from "rc-slider"
+import Tooltip from "rc-slider"
+import SliderWithTooltip from "rc-slider"
+
 import {
   select,
   scaleLinear,
@@ -11,6 +15,8 @@ import {
   easeCubic,
   easeLinear,
 } from "d3"
+import "rc-slider/assets/index.css"
+import "rc-tooltip/assets/bootstrap.css"
 
 function GlacierMeltOverview() {
   //transaltion
@@ -18,14 +24,38 @@ function GlacierMeltOverview() {
 
   //svg sizing
   const svgRef = useRef()
-  const width = 700
-  const height = 550
-  const margin = 98
-  const mountainHeight = 2200
 
   //state
   const [showAnswer, setShowAnswer] = useState(false)
   const [nextPage, setNextPage] = useState(false)
+  const [percentageLabel, setPercentageLabel] = useState({
+    percentage: 10,
+    volumePercentage: 13,
+  })
+  const [dataVolume, setDataVolume] = useState({
+    data_1850: 130,
+    data_2019: 50.645,
+  })
+
+  function handleSliderChange(p) {
+    setPercentageLabel({
+      percentage: p,
+      volumePercentage: (dataVolume.data_1850 * p) / 100,
+    })
+  }
+
+  function showPercentageLabel() {
+    return (
+      <div>
+        <p>{percentageLabel.percentage} % Rückgang</p>
+        <p>
+          {percentageLabel.volumePercentage} km<sup>3</sup> Rückgang
+        </p>
+      </div>
+    )
+  }
+
+  function showVolumeLabel() {}
 
   /**
    * Adds Speach Bubble with text for Globe
@@ -43,7 +73,12 @@ function GlacierMeltOverview() {
             Die Gletscher schmelzen immer mehr. Um wieviel ist der Gletscher im
             Jahr 2019 seit 1850 zurückgegeangen?
           </p>
-          
+          <Slider
+            value={percentageLabel.percentage}
+            onChange={handleSliderChange}
+          />
+          {showPercentageLabel()}
+          {showVolumeLabel()}
         </div>
       </CSSTransition>
     )
@@ -53,7 +88,7 @@ function GlacierMeltOverview() {
     <React.Fragment>
       <div className="glacier-container">
         {createBubbleStartQuizz()}
-        <svg className="glacier-graph" width={width + 2 * margin}>
+        <svg className="glacier-graph" width={500}>
           <g ref={svgRef}></g>
         </svg>
       </div>
