7e96870e5af768a08dc4074758e281cfd4b8fefa
Tabea <tabea.eggler@students.fhnw.ch>
2021-06-11 14:11:55 +0200

    test start - globe

--- src/routing/Routes.js
+++ src/routing/Routes.js
@@ -1,28 +1,23 @@
 import React from "react"
-import { Router, Switch, Route, useRouterHistory } from "react-router-dom"
+import { Router, Switch, Route } from "react-router-dom"
 import Snowline from "../components/climate2/Climate2"
 import Climate1 from "../components/climate1/Climate1"
 import Cause from "../components/climate3/Climate3"
-//import history from "./history"
+import history from "./history"
 import Start from "../components/Start"
 import End from "../components/End"
-
-import { createBrowserHistory} from 'history';
 
 /**
  * Rendering of components with specific path
  * @param {function} props.setPageNr setter for navigation
  */
 const Routes = props => {
-  const hist = useRouterHistory(createBrowserHistory)({
-    basename: '/',
-})
   return (
     <div>
-      <Router history={hist}>
+      <Router history={history}>
         <Switch>
-          <Route path={process.env.PUBLIC_URL} exact component={() => <Start setPageNr={props.setPageNr}/>} />
-          <Route path={process.env.PUBLIC_URL + "Globe"} exact component={() => <Climate1 setPageNr={props.setPageNr} />} />
+          <Route path={process.env.PUBLIC_URL} exact component={() => <Climate1 setPageNr={props.setPageNr}/>} />
+          <Route path={process.env.PUBLIC_URL + "Globe"} exact component={() => <Start setPageNr={props.setPageNr} />} />
           <Route path={process.env.PUBLIC_URL + "Snowline"} component={() => <Snowline setPageNr={props.setPageNr} />} />
           <Route path={process.env.PUBLIC_URL + "Cause"} component={() => <Cause setPageNr={props.setPageNr} />} />
           <Route path={process.env.PUBLIC_URL + "End"} component={() => <End setPageNr={props.setPageNr} />} />
