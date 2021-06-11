245deddcd7e880883cdb44fa53b4dfcaea646f21
Tabea <tabea.eggler@students.fhnw.ch>
2021-06-11 14:46:40 +0200

    repeat

--- src/i18n.js
+++ src/i18n.js
@@ -22,7 +22,7 @@ i18n
       loadPath: () => {
         // check the domain
         const host = window.location.host
-        return (host === "production.ltd" ? "/static/app" : "") + "/static/app/static/locales/{{lng}}/{{ns}}.json"
+        return (host === "production.ltd" ? "/static/app" : "") + "/mission-erde/klima/static/locales/{{lng}}/{{ns}}.json"
       },
     },
     lng: "de",
